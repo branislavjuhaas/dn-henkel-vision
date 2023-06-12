7814a75edbc8ea044ccfcdd652ecec6b51f2068d
BrancheyJuhas <juhas.branislaw@gmail.com>
2023-06-12 16:54:06 +0200

    Felber Training Finished 120623

--- DN Henkel Vision/DN Henkel Vision/Memory/Order.cs
+++ DN Henkel Vision/DN Henkel Vision/Memory/Order.cs
@@ -8,8 +8,21 @@ namespace DN_Henkel_Vision.Memory
 {
     public class Order
     {
+        #region Export Constants
+
+        public const int Complete = 0;
+        public const int Exported = 1;
+        public const int Partial = 2;
+
+        #endregion
+
         public string OrderNumber;
         public string Faults;
+        
+        public int OrderTotalTime;
+        public int ExportModifiedTime;
+
+        public int State = Complete;
 
         /// <summary>
         /// This Bool checks if the order is a Netstal order.
