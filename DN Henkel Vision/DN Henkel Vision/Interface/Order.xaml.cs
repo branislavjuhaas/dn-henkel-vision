d16c4418d9d027221cfad3baada8689eaa844732
Branislav Juhás <juhas.branislaw@gmail.com>
2023-07-10 12:44:19 +0200

    Order Creation Fixed 100723

--- DN Henkel Vision/DN Henkel Vision/Interface/Order.xaml.cs
+++ DN Henkel Vision/DN Henkel Vision/Interface/Order.xaml.cs
@@ -33,37 +33,30 @@ namespace DN_Henkel_Vision.Interface
 
         private void Number_KeyUp  (object sender, KeyRoutedEventArgs e)
         {
-            if (e.Key != Windows.System.VirtualKey.Delete && e.Key != Windows.System.VirtualKey.Back)
+            string order = Environment.Format(Number.Text.Replace(" ", ""));
+
+            if (Manager.OrdersRegistry.Contains(order))
             {
-                int position = Number.Text.Length - Number.SelectionStart;
-
-                Number.Text = Environment.Format(Number.Text.Replace(" ", ""));
-
-                Number.SelectionStart = Number.Text.Length - position;
-            }
-
-            if (Manager.OrdersRegistry.Contains(Number.Text))
-            {
-                CategoryChip.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 209, 94, 94));
+                //CategoryChip.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 209, 94, 94));
                 CategoryText.Text = "Existing";
                 return;
             }
 
-            if (Number.Text.StartsWith("20") && Number.Text.Length == 10)
+            if (Number.Text.StartsWith("20") && order.Length == 10)
             {
-                CategoryChip.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 33, 135, 33));
+                //CategoryChip.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 33, 135, 33));
                 CategoryText.Text = "Netstal";
                 return;
             }
 
-            if (Number.Text.StartsWith("38") && Number.Text.Length == 10)
+            if (order.StartsWith("38") && order.Length == 10)
             {
-                CategoryChip.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 33, 135, 33));
+                //CategoryChip.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 33, 135, 33));
                 CategoryText.Text = "Auftrag";
                 return;
             }
 
-            CategoryChip.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 209, 94, 94));
+            //CategoryChip.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 209, 94, 94));
             CategoryText.Text = "Invalid";
         }
     }
