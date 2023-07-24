8f6b7d94fa61901b3e6d1ec79999c6a7489b75e7
Branislav Juhás <juhas.branislaw@gmail.com>
2023-07-24 15:20:06 +0200

    Cause Update 240723

--- DN Henkel Vision/DN Henkel Vision/AssemblyInfo.cs
+++ DN Henkel Vision/DN Henkel Vision/AssemblyInfo.cs
@@ -5,6 +5,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("2023.2.1.1437")]
-[assembly: AssemblyFileVersion("2023.2.1.1437")]
-[assembly: AssemblyInformationalVersion("2023.2.1.1437")]
+[assembly: AssemblyVersion("2023.2.1.1438")]
+[assembly: AssemblyFileVersion("2023.2.1.1438")]
+[assembly: AssemblyInformationalVersion("2023.2.1.1438")]
